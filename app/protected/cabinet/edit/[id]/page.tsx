fe16f3f5bcb02bd439a44e182f8903c3b9fb4450
Edwin Silvestre <a02305847@aggies.usu.edu>
2024-04-02 21:53:32 -0600

    Type error fix

--- app/protected/cabinet/edit/[id]/page.tsx
+++ app/protected/cabinet/edit/[id]/page.tsx
@@ -21,14 +21,14 @@ export default async function CabinetEditPage({ params}: { params : { id: string
 
 
     return(
-        <>
-        <div className="flex-1 w-full flex flex-col gap-20 items-center">
-            <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-                <main className="flex-1 flex flex-col gap-6">
-                  <CabinetEditForm cabinet={cabinet} id={id}/>
-                </main>
-            </div>
+      <>
+      <div className="flex-1 w-full flex flex-col gap-20 items-center">
+        <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
+          <main className="flex-1 flex flex-col gap-6">
+            <CabinetEditForm cabinet={cabinet || []} id={id}/>
+          </main>
         </div>
-        </>
+      </div>
+      </>
     )
 }
