79dfe3915f1ba7bf263c85fa1f0a939b2eebe9b9
Edwin Silvestre <a02305847@aggies.usu.edu>
2024-04-03 10:36:38 -0600

    Fix login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
-
+import ProtectedPage from "@/app/protected/page";
 export default async function Index() {
  const supabase = createClient();
 
@@ -9,33 +8,9 @@ export default async function Index() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">        
-        {user ? (<> <AuthButton /></>) : <><p>SingIn</p><AuthButton /></>}
-        </div>
-      </nav>
-
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-
-
-        </main>
+            {user ? <ProtectedPage/> : "Sigin in to see your cabinets"}
       </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href={`${process.env.MY_WEBSITE}`}
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-          Edwin Silvestre
-          </a>
-        </p>
-      </footer>
     </div>
   );
 }
